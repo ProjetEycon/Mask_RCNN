d8129d38092acea19951e0f5145c40e9a9ef75c0
Salim Aissi <saissi1701@gmail.com>
2023-06-07 09:20:24 +0200

    KL

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -16,12 +16,12 @@ import logging
 from collections import OrderedDict
 import multiprocessing
 import numpy as np
-import tensorflow as tf
-import keras
-import keras.backend as K
-import keras.layers as KL
-import keras.engine as KE
-import keras.models as KM
+import tensorflow.compat.v1 as tf
+import tensorflow.compat.v1.keras
+import tensorflow.compat.v1.keras.backend as K
+import tensorflow.compat.v1.keras.layers as KL
+import tensorflow.compat.v1.keras.engine as KE
+import tensorflow.compat.v1.keras.models as KM
 
 from mrcnn import utils
 
